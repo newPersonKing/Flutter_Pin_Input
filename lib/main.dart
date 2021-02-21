70de779273167d249a2ad59acd5df46dde5c1078
郭勇 <>
2021-02-21 14:18:13 +0800

    pin_input

---
+++ lib/main.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pinput/pin_put.dart';
+
+void main() => runApp(PinPutApp());
+
+class PinPutApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: PinPutView()),
+    );
+  }
+}
+
+class PinPutView extends StatefulWidget {
+  @override
+  PinPutViewState createState() => PinPutViewState();
+}
+
+class PinPutViewState extends State<PinPutView> {
+  final _formKey = GlobalKey<FormState>();
+  final _pinPutController = TextEditingController();
+  final _pinPutFocusNode = FocusNode();
+  final _pageController = PageController();
+
+  int _pageIndex = 0;
+
+  final List<Widget> _pinPuts = [];
+
+  final List<Color> _bgColors = [
+    Colors.white,
+    const Color.fromRGBO(43, 36, 198, 1),
+    Colors.white,
+    const Color.fromRGBO(75, 83, 214, 1),
+    const Color.fromRGBO(43, 46, 66, 1),
+  ];
+
+  @override
+  void initState() {
+    _pinPuts.addAll([
+      onlySelectedBorderPinPut(),
+      darkRoundedPinPut(),
+      animatingBorders(),
+      boxedPinPutWithPreFilledSymbol(),
+      justRoundedCornersPinPut(),
+    ]);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.passthrough,
+      children: <Widget>[
+        AnimatedContainer(
+          color: _bgColors[_pageIndex],
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.all(40.0),
+          child: PageView(
+            scrollDirection: Axis.vertical,
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() => _pageIndex = index);
+            },
+            children: _pinPuts.map((p) {
+              return FractionallySizedBox(
+                heightFactor: 1.0,
+                child: Center(child: p),
+              );
+            }).toList(),
+          ),
+        ),
+        _bottomAppBar,
+      ],
+    );
+  }
+
+  Widget onlySelectedBorderPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(235, 236, 237, 1),
+      borderRadius: BorderRadius.circular(5.0),
+    );
+    return Form(
+      key: _formKey,
+      child: GestureDetector(
+        onLongPress: () {
+          print(_formKey.currentState.validate());
+        },
+        child: PinPut(
+          validator: (s) {
+            if (s.contains('1')) return null;
+            return 'NOT VALID';
+          },
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          withCursor: true,
+          fieldsCount: 5,
+          fieldsAlignment: MainAxisAlignment.spaceAround,
+          textStyle: const TextStyle(fontSize: 25.0, color: Colors.black),
+          eachFieldMargin: EdgeInsets.all(0),
+          eachFieldWidth: 45.0,
+          eachFieldHeight: 55.0,
+          onSubmit: (String pin) => _showSnackBar(pin),
+          focusNode: _pinPutFocusNode,
+          controller: _pinPutController,
+          submittedFieldDecoration: pinPutDecoration,
+          selectedFieldDecoration: pinPutDecoration.copyWith(
+            color: Colors.white,
+            border: Border.all(
+              width: 2,
+              color: const Color.fromRGBO(160, 215, 220, 1),
+            ),
+          ),
+          followingFieldDecoration: pinPutDecoration,
+          pinAnimationType: PinAnimationType.scale,
+        ),
+      ),
+    );
+  }
+
+  Widget darkRoundedPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(25, 21, 99, 1),
+      borderRadius: BorderRadius.circular(20.0),
+    );
+    return PinPut(
+      eachFieldWidth: 65.0,
+      eachFieldHeight: 65.0,
+      withCursor: true,
+      fieldsCount: 4,
+      focusNode: _pinPutFocusNode,
+      controller: _pinPutController,
+      onSubmit: (String pin) => _showSnackBar(pin),
+      submittedFieldDecoration: pinPutDecoration,
+      selectedFieldDecoration: pinPutDecoration,
+      followingFieldDecoration: pinPutDecoration,
+      pinAnimationType: PinAnimationType.scale,
+      textStyle: const TextStyle(color: Colors.white, fontSize: 20.0),
+    );
+  }
+
+  Widget animatingBorders() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      border: Border.all(color: Colors.deepPurpleAccent),
+      borderRadius: BorderRadius.circular(15.0),
+    );
+    return PinPut(
+      fieldsCount: 5,
+      eachFieldHeight: 40.0,
+      withCursor: true,
+      onSubmit: (String pin) => _showSnackBar(pin),
+      focusNode: _pinPutFocusNode,
+      controller: _pinPutController,
+      submittedFieldDecoration: pinPutDecoration.copyWith(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      selectedFieldDecoration: pinPutDecoration,
+      followingFieldDecoration: pinPutDecoration.copyWith(
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(
+          color: Colors.deepPurpleAccent.withOpacity(.5),
+        ),
+      ),
+    );
+  }
+
+  Widget boxedPinPutWithPreFilledSymbol() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(119, 125, 226, 1),
+      borderRadius: BorderRadius.circular(5.0),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(3.0),
+        border: Border.all(color: Colors.white),
+      ),
+      padding: const EdgeInsets.all(20.0),
+      child: PinPut(
+        withCursor: true,
+        fieldsCount: 5,
+        preFilledWidget: FlutterLogo(),
+        textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+        eachFieldWidth: 50.0,
+        eachFieldHeight: 50.0,
+        onSubmit: (String pin) => _showSnackBar(pin),
+        focusNode: _pinPutFocusNode,
+        controller: _pinPutController,
+        submittedFieldDecoration: pinPutDecoration,
+        selectedFieldDecoration: pinPutDecoration.copyWith(color: Colors.white),
+        followingFieldDecoration: pinPutDecoration,
+      ),
+    );
+  }
+
+  Widget justRoundedCornersPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(43, 46, 66, 1),
+      borderRadius: BorderRadius.circular(10.0),
+      border: Border.all(
+        color: const Color.fromRGBO(126, 203, 224, 1),
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: PinPut(
+        fieldsCount: 4,
+        withCursor: true,
+        textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+        eachFieldWidth: 40.0,
+        eachFieldHeight: 55.0,
+        onSubmit: (String pin) => _showSnackBar(pin),
+        focusNode: _pinPutFocusNode,
+        controller: _pinPutController,
+        submittedFieldDecoration: pinPutDecoration,
+        selectedFieldDecoration: pinPutDecoration,
+        followingFieldDecoration: pinPutDecoration,
+        pinAnimationType: PinAnimationType.fade,
+      ),
+    );
+  }
+
+  Widget get _bottomAppBar {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FlatButton(
+            onPressed: () => _pinPutFocusNode.requestFocus(),
+            child: const Text('Focus'),
+          ),
+          FlatButton(
+            onPressed: () => _pinPutFocusNode.unfocus(),
+            child: const Text('Unfocus'),
+          ),
+          FlatButton(
+            onPressed: () => _pinPutController.text = '',
+            child: const Text('Clear All'),
+          ),
+          FlatButton(
+            child: Text('Paste'),
+            onPressed: () => _pinPutController.text = '234',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSnackBar(String pin) {
+    final snackBar = SnackBar(
+      duration: const Duration(seconds: 3),
+      content: Container(
+        height: 80.0,
+        child: Center(
+          child: Text(
+            'Pin Submitted. Value: $pin',
+            style: const TextStyle(fontSize: 25.0),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.deepPurpleAccent,
+    );
+    Scaffold.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+}
---
+++ lib/pin_put_state.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_pinput/pin_put.dart';
+
+class PinPutState extends State<PinPut>
+    with WidgetsBindingObserver, SingleTickerProviderStateMixin {
+  TextEditingController _controller;
+  FocusNode _focusNode; /*隐藏的TextFiled的焦点*/
+  ValueNotifier<String> _textControllerValue;
+
+  int get selectedIndex => _controller.value.text.length;
+
+  Animation _cursorAnimation;
+  AnimationController _cursorAnimationController;
+
+  @override
+  void initState() {
+    _controller = widget.controller ?? TextEditingController();
+    _focusNode = widget.focusNode ?? FocusNode();
+    _textControllerValue = ValueNotifier<String>(_controller.value.text);
+    _controller?.addListener(_textChangeListener);
+    _focusNode?.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    if (widget.withCursor) {
+      _cursorAnimationController = AnimationController(
+          vsync: this, duration: Duration(milliseconds: 500));
+      _cursorAnimation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+          curve: Curves.linear, parent: _cursorAnimationController));
+
+      _cursorAnimationController.addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed) {
+          _cursorAnimationController.repeat(reverse: true);
+        }
+      });
+      _cursorAnimationController.forward();
+    }
+
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+    _focusNode.requestFocus();
+  }
+
+  void _textChangeListener() {
+    final pin = _controller.value.text;
+    widget.onChanged?.call(pin);
+    if (pin != _textControllerValue.value) {
+      try {
+        _textControllerValue.value = pin;
+      } catch (e) {
+        _textControllerValue = ValueNotifier(_controller.value.text);
+      }
+      if (pin.length == widget.fieldsCount) widget.onSubmit?.call(pin);
+    }
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) _controller.dispose();
+    if (widget.focusNode == null) _focusNode.dispose();
+
+    _cursorAnimationController?.dispose();
+    _textControllerValue?.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState appLifecycleState) {
+    if (appLifecycleState == AppLifecycleState.resumed) {
+      _checkClipboard();
+    }
+  }
+
+  Future<void> _checkClipboard() async {
+    final ClipboardData clipboardData = await Clipboard.getData('text/plain');
+    if (clipboardData?.text?.length == widget.fieldsCount) {
+      widget.onClipboardFound?.call(clipboardData.text);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _hiddenTextField,
+        _fields,
+      ],
+    );
+  }
+
+  void _handleTap() {
+    // final focus = FocusScope.of(context);
+    // if (_focusNode.hasFocus) _focusNode.unfocus();
+    // if (focus.hasFocus) focus.unfocus();
+    // focus.requestFocus(FocusNode());
+    // Future.delayed(Duration.zero, () => focus.requestFocus(_focusNode));
+    // if (widget.onTap != null) widget.onTap();
+  }
+
+  /*隐藏的TextField*/
+  Widget get _hiddenTextField {
+    return TextFormField(
+      controller: _controller,
+      onTap: widget.onTap,
+      onSaved: widget.onSaved,
+      onChanged: widget.onChanged,
+      validator: widget.validator,
+      autovalidateMode: widget.autovalidateMode,
+      textInputAction: widget.textInputAction,
+      focusNode: _focusNode,
+      enabled: widget.enabled,
+      enableSuggestions: false,
+      autofocus: widget.autofocus,
+      obscureText: widget.obscureText != null,
+      autocorrect: false,
+      keyboardAppearance: widget.keyboardAppearance,
+      keyboardType: widget.keyboardType,
+      textCapitalization: widget.textCapitalization,
+      inputFormatters: widget.inputFormatters,
+      enableInteractiveSelection: false,
+      maxLength: widget.fieldsCount,
+      showCursor: false,
+      scrollPadding: EdgeInsets.zero,
+      decoration: widget.inputDecoration,
+      style: widget.textStyle != null
+          ? widget.textStyle.copyWith(color: Colors.transparent)
+          : const TextStyle(color: Colors.transparent),
+    );
+  }
+
+  Widget get _fields {
+    return ValueListenableBuilder<String>(
+      valueListenable: _textControllerValue,
+      builder: (BuildContext context, value, Widget child) {
+        return GestureDetector(
+          onTap: _handleTap,
+          child: Row(
+            mainAxisSize: widget.mainAxisSize,
+            mainAxisAlignment: widget.fieldsAlignment,
+            children: _buildFieldsWithSeparator(),
+          ),
+        );
+      },
+    );
+  }
+
+  List<Widget> _buildFieldsWithSeparator() {
+    final fields = Iterable<int>.generate(widget.fieldsCount).map((index) {
+      return _getField(index);
+    }).toList();
+
+    /*todo 这个可以不需要*/
+    // for (final int i in widget.separatorPositions) {
+    //   if (i <= widget.fieldsCount) {
+    //     final List<int> smaller =
+    //         widget.separatorPositions.where((int d) => d < i).toList();
+    //     fields.insert(i + smaller.length, widget.separator);
+    //   }
+    // }
+
+    return fields;
+  }
+
+  Widget _getField(int index) {
+    final String pin = _controller.value.text;
+    return AnimatedContainer(
+      width: widget.eachFieldWidth,
+      height: widget.eachFieldHeight,
+      alignment: widget.eachFieldAlignment,
+      duration: widget.animationDuration,
+      curve: widget.animationCurve,
+      padding: widget.eachFieldPadding,
+      margin: widget.eachFieldMargin,
+      constraints: widget.eachFieldConstraints,
+      decoration: _fieldDecoration(index),
+      /*当子组件改变的时候 会有动画效果*/
+      child: AnimatedSwitcher(
+        switchInCurve: widget.animationCurve,
+        switchOutCurve: widget.animationCurve,
+        duration: widget.animationDuration,
+        transitionBuilder: (child, animation) {
+          return _getTransition(child, animation);
+        },
+        child: _buildFieldContent(index, pin),
+      ),
+    );
+  }
+
+  Widget _buildFieldContent(int index, String pin) {
+    if (index < pin.length) {
+      return Text(
+        widget.obscureText ?? pin[index],
+        key: ValueKey<String>(index < pin.length ? pin[index] : ''),
+        style: widget.textStyle,
+      );
+    }
+
+    /*获取焦点的pin*/
+    if (widget.withCursor && _focusNode.hasFocus && index == pin.length) {
+      return _buildCursor();
+    }
+
+    /*自定义默认显示样式*/
+    if (widget.preFilledWidget != null)
+      return SizedBox(
+        key: ValueKey<String>(index < pin.length ? pin[index] : ''),
+        child: widget.preFilledWidget,
+      );
+
+    return Text(
+      '',
+      key: ValueKey<String>(index < pin.length ? pin[index] : ''),
+      style: widget.textStyle,
+    );
+  }
+
+  BoxDecoration _fieldDecoration(int index) {
+    if (!widget.enabled) return widget.disabledDecoration;
+    if (index < selectedIndex && _focusNode.hasFocus) {
+      return widget.submittedFieldDecoration;
+    }
+    if (index == selectedIndex && _focusNode.hasFocus) {
+      return widget.selectedFieldDecoration;
+    }
+    return widget.followingFieldDecoration;
+  }
+
+  Widget _getTransition(Widget child, Animation animation) {
+    switch (widget.pinAnimationType) {
+      case PinAnimationType.none:
+        return child;
+      case PinAnimationType.fade:
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      case PinAnimationType.scale:
+        return ScaleTransition(
+          scale: animation,
+          child: child,
+        );
+      case PinAnimationType.slide:
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: widget.slideTransitionBeginOffset ?? Offset(0.8, 0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: child,
+        );
+      case PinAnimationType.rotation:
+        return RotationTransition(
+          turns: animation,
+          child: child,
+        );
+    }
+    return child;
+  }
+
+  Widget _buildCursor() {
+    return AnimatedBuilder(
+      animation: _cursorAnimationController,
+      builder: (context, child) {
+        return Center(
+          child: Opacity(
+            opacity: _cursorAnimation.value,
+            child: widget.cursor ?? Text('|', style: widget.textStyle),
+          ),
+        );
+      },
+    );
+  }
+}
